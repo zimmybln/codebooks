3c99450232ad4d03654a2c241e14f937fd6a2167
zimmybln <zimmy@web.de>
2015-06-29 09:29:07 +0200

    Erste Fassung

---
+++ Samples Allgemein/ExpressionHouse/ExpressionHouse/SimpleExpressions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ExpressionHouse
+{
+    [TestFixture]
+    public class SimpleExpressions
+    {
+
+        [Test]
+        public void ExecuteAnalyze()
+        {
+            var analyzer = new AnalyzeExpression();
+
+
+            analyzer.Analyze(() => DateTime.Now.ToString());
+            
+
+        }
+
+
+
+    }
+}
